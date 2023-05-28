3999c59183b67e5860068bc049addc73444003ba
Kyle Houghton <kyle@keentocode.dev>
2023-05-28 02:45:56 -0500

    Finished triangle.rb.

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,14 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  {0 => :scalene, 1 => :isosceles, 3 => :equilateral}[[a == b, a == c, b == c].count(true)]
+  sorted = [a, b, c].sort
+  lengths_are_over_zero = sorted.any? {|e| e > 0}
+  positive_area = sorted[0] + sorted[1] > sorted[2]
+  unless lengths_are_over_zero && positive_area
+    raise TriangleError
+  else
+    {0 => :scalene, 1 => :isosceles, 3 => :equilateral}[[a == b, a == c, b == c].count(true)]
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
